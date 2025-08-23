4c56b0348dc99394390240e95a4b93eaff851a36
leti74 <letiimar4@gmail.com>
2025-08-23 17:29:36 +0200

    git pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,28 +9,33 @@ import { CercaRicetta } from "./pages/CercaRicetta.jsx";
 import { RicettaSelezionata } from "./pages/RicettaSelezionata.jsx";
 import { ElencoRicetteProvider } from "./stores/ElencoRicetteContext.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/perche",
+      element: <Perche />,
+    },
+    {
+      path: "/perIngrediente",
+      element: <RicettePerIngrediente />,
+    },
+    {
+      path: "/cerca",
+      element: <CercaRicetta />,
+    },
+    {
+      path: "/:ricettaID",
+      element: <RicettaSelezionata />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/perche",
-    element: <Perche />,
-  },
-  {
-    path: "/perIngrediente",
-    element: <RicettePerIngrediente />,
-  },
-  {
-    path: "/cerca",
-    element: <CercaRicetta />,
-  },
-  {
-    path: "/:ricettaID",
-    element: <RicettaSelezionata />,
-  },
-]);
+    basename: "/ricette-vegetariane/",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ElencoRicetteProvider>
