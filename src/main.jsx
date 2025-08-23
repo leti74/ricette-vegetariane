2f15a28cca941a3778b8256d91235d91a151d92d
leti74 <letiimar4@gmail.com>
2025-08-23 18:03:10 +0200

    cambiato routint

--- src/main.jsx
+++ src/main.jsx
@@ -2,45 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./pages/App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Perche } from "./pages/Perche.jsx";
 import { RicettePerIngrediente } from "./pages/RicettePerIngrediente.jsx";
 import { CercaRicetta } from "./pages/CercaRicetta.jsx";
 import { RicettaSelezionata } from "./pages/RicettaSelezionata.jsx";
 import { ElencoRicetteProvider } from "./stores/ElencoRicetteContext.jsx";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "/perche",
-      element: <Perche />,
-    },
-    {
-      path: "/perIngrediente",
-      element: <RicettePerIngrediente />,
-    },
-    {
-      path: "/cerca",
-      element: <CercaRicetta />,
-    },
-    {
-      path: "/:ricettaID",
-      element: <RicettaSelezionata />,
-    },
-  ],
-  {
-    basename: "/ricette-vegetariane/",
-  }
-);
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ElencoRicetteProvider>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/perche" element={<Perche />} />
+          <Route path="/perIngrediente" element={<RicettePerIngrediente />} />
+          <Route path="/cerca" element={<CercaRicetta />} />
+          <Route path="/:ricettaID" element={<RicettaSelezionata />} />
+        </Routes>
+      </HashRouter>
     </React.StrictMode>
   </ElencoRicetteProvider>
 );
